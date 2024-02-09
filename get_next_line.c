ceb49fdfe4da1c50d5f5d9a5f4bf5ee7c1dcc1ea
tokochiz <tokohana.1003@gmail.com>
2024-02-09 18:32:35 +0900

    :sparkles:add: 新規ファイル追加（メモリリーク未対応）

--- get_next_line.c
+++ get_next_line.c
@@ -5,124 +5,108 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By:  ctokoyod < ctokoyod@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/04 14:33:36 by  ctokoyod         #+#    #+#             */
-/*   Updated: 2024/02/09 16:17:11 by  ctokoyod        ###   ########.fr       */
+/*   Created: 2024/02/09 16:45:06 by  ctokoyod         #+#    #+#             */
+/*   Updated: 2024/02/09 18:26:42 by  ctokoyod        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-// メモリ領域を解放するための関数
-static void	release_memory_area(char **line)
-{
-	if (*line == NULL)
-		return ;
-	free(*line);
-	*line = NULL;
-}
+// char	*releace_memory(char *line)
+// {
+// }
 
-static char	*find_one_line(char *line)
+static char	*get_line_from_save(char **save)
 {
+	char	*line;
+	char	*next_line;
 	int		i;
-	char	*remaining_text;
 
-	i = 0;
-	while (line[i] != '\n' && line[i] != '\0')
-		i++;
-	if (line[i] == '\0')
-		return (NULL);
-	// remaining_text = ft_strdup(line + i + 1);
-	remaining_text = ft_substr(line, i + 1, (ft_strlen(line) - i));
-	if (*remaining_text == '\0') // もし抽出した部分が空だったら
+	if (ft_strchr(*save, '\n') == NULL)
 	{
-		return NULL;
+		line = ft_strdup(*save);
+		free(*save);
+		*save = NULL;
+		return (line);
 	}
-	line[i + 1] = '\0'; // 元の文字列に終端文字を設定する
-	return (remaining_text);
+	else
+	{
+		i = 0;
+		while ((*save)[i] != '\n')
+			i++;
+		line = ft_substr(*save, 0, i + 1);
+		next_line = ft_strdup(*save + i + 1);
+		free(*save);
+		*save = next_line;
+		return (line);
+	}
 }
 
-static char	*read_fd(int fd, char *buffer, char **line)
+static char	*read_file(int fd, char *buffer, char *joined)
 {
-	char	*tmp_line;
 	ssize_t	read_bytes;
 
 	read_bytes = 1;
-	while ((read_bytes) != 0)
+	while (read_bytes > 0 && !strchr(joined, '\n'))
 	{
 		read_bytes = read(fd, buffer, BUFFER_SIZE);
 		if (read_bytes == -1)
 			return (NULL);
 		if (read_bytes == 0)
 			break ;
-		buffer[read_bytes] = '\0'; // バッファの終端を設定
-		if (*line == NULL)         // 既存のlineがからの場合とNULLの場合を考慮
-			*line = ft_strdup(buffer);
-		else
-		{
-			tmp_line = ft_strjoin(*line, buffer);
-			if (tmp_line == NULL)
-				return (NULL);
-			free(*line);
-			*line = tmp_line;
-			if (ft_strchr(buffer, '\n'))
-				break ;
-		}
+		buffer[read_bytes] = '\0';
+		joined = ft_strjoin(joined, buffer);
 	}
+	if (read_bytes == 0 && ft_strlen(joined) == 0)
+		return (NULL);
 	free(buffer);
-	return (*line);
+	return (joined);
 }
 
 char	*get_next_line(int fd)
 {
-	char		*line;
 	static char	*save;
 	char		*buffer;
+	char		*joined;
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (NULL); // ERROR処理
-	buffer = malloc(BUFFER_SIZE + 1);
+	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (buffer == NULL)
-		return (NULL); // ERROR処理
-	if (save == NULL)
-		save = ft_strdup("");
-	line = read_fd(fd, buffer, &save);
-	// free(buffer);
-	if (line < 0)
+		return (NULL);
+	joined = strdup("");
+	if (joined == NULL)
 	{
-		release_memory_area(&line);
-	}
-	if (line == 0 && ft_strlen(line) == 0)
-	{
-		release_memory_area(&line);
+		free(buffer);
 		return (NULL);
 	}
-	return (find_one_line(line));
+	if (save == NULL)
+		save = read_file(fd, buffer, joined);
+	if (save == NULL)
+		return (NULL);
+	return (get_line_from_save(&save));
 }
 
-int	main(void)
-{
-	int fd;
-	char *line;
+// int	main(void)
+// {
+// 	int fd;
+// 	char *line;
 
-	// テキストファイルを開く
-	fd = open("test.txt", O_RDONLY);
-	if (fd < 0)
-	{
-		perror("Failed to open file");
-		return (1);
-	}
+// 	// テキストファイルを開く
+// 	fd = open("test1.txt", O_RDONLY);
+// 	if (fd < 0)
+// 	{
+// 		perror("Failed to open file");
+// 		return (1);
+// 	}
 
-	// ファイルの内容を1行ずつ読み込む
-	while ((line = get_next_line(fd)))
-	{
-		printf("%s", line);
-		free(line);
-	}
+// 	// ファイルの内容を1行ずつ読み込む
+// 	while ((line = get_next_line(fd)))
+// 	{
+// 		printf("%s", line);
+// 		free(line);
+// 	}
 
-	// ファイルを閉じる
-	close(fd);
+// 	// ファイルを閉じる
+// 	close(fd);
 
-	return (0);
-}
-
-
+// 	return (0);
+// }
